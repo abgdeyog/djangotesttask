525291da2b7e49a92c4600156018c32bc09ffe52
abgdeyog <abgdeyog@gmail.com>
2020-02-28 19:23:13 +0300

    fix

--- project/settings.py
+++ project/settings.py
@@ -122,5 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/home/ubuntu/testtask/djangotesttask/static',
+]
 
