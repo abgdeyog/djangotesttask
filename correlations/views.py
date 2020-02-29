499188e97e013618cd7dfa3b13b0685827536d02
abgdeyog <abgdeyog@gmail.com>
2020-02-29 16:12:46 +0300

    test

--- correlations/views.py
+++ correlations/views.py
@@ -15,6 +15,7 @@ from scipy.stats import pearsonr
 class CorrelationView(APIView):
 
     def get(self, request):
+        return Response({"message": "can not retrieve the results"}, status=status.HTTP_400_BAD_REQUEST)
         try:
             coins = json.loads(request.GET["coins"]).lower()
             coins = re.findall(r'\w+', coins)
@@ -37,7 +38,6 @@ class CorrelationView(APIView):
             for coin in coins:
                 data_by_coins[coin].append(data_by_day[coin]["close"])
         # except:
-        #     return Response({"message": "can not retrieve the results"}, status=status.HTTP_400_BAD_REQUEST)
         coins_correlations = {}
         for coin in coins:
             coin_correlation = {}
